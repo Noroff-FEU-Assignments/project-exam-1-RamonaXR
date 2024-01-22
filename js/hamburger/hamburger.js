44d48294577dbe281ec9a86ba0d6b162eb7541ef
RamonaXR <Ramonaelise@gmail.com>
2024-01-22 22:08:29 +0100

    hamburger menu

--- js/hamburger/hamburger.js
+++ js/hamburger/hamburger.js
@@ -1,8 +1,33 @@
 /* Show hamburgermenu*/
 
+
+const menuButton = document.querySelector(".hamburger-wrap");
+    const closingButton = document.querySelector(".closing-btn");
+    const navElement = document.querySelector("nav");
+    const overlay = document.querySelector(".overlay");
+
 export function toggleMenu() {
-    const menu = document.querySelector('.nav-head');
-    menu.classList.toggle('show');
+    
+
+    menuButton.addEventListener("click", function() {
+        navElement.classList.remove("hide");
+        overlay.classList.remove("hide");
+        setTimeout(menuAction, 100);
+    })
+
 }
 
-document.querySelector('.hamburger').addEventListener('click', toggleMenu);
+    closingButton.addEventListener("click", closeMenu);
+    overlay.addEventListener("click", closeMenu);
+
+
+export function closeMenu() {
+    navElement.classList.add("hide");
+    navElement.style.right = "-150px";
+    overlay.classList.add("hide");
+}
+
+export function menuAction() {
+    navElement.style.right = "0px";
+    navElement.style.transition = "500ms";
+}
