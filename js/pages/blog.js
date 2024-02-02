338ca5d3cc64b3828971fafe1f38b7eaf3cfaae0
RamonaXR <Ramonaelise@gmail.com>
2024-02-02 12:40:43 +0100

    More advanced styling of blogposts

--- js/pages/blog.js
+++ js/pages/blog.js
@@ -2,9 +2,12 @@ import { url } from "../data/constants.js";
 import { fetchData } from "../data/fetchApi.js";
 import { renderBlog } from "../rendering/renderBlog.js";
 
+const parent = document.querySelector(".blogposts") 
+
 export async function createBlog(){
     try {
         const posts = await fetchData(url);
+        parent.innerHTML = "";
         renderBlog(posts); 
     } catch (error) {
         console.log(error); // ADD BETTER ERROR 
