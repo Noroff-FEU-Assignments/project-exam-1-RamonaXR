f1ae1146d5ac8c190247d7d4bae8c4f765f5a9fb
RamonaXR <Ramonaelise@gmail.com>
2024-02-02 12:57:38 +0100

    posts home

--- js/rendering/renderHome.js
+++ js/rendering/renderHome.js
@@ -7,42 +7,64 @@ export function renderHome(posts){
    
    
 
-    /*while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }*/
+
 
     for (let i = 0; i < 4; i++){
+        
         const element = document.createElement("article");
         element.classList.add("posts");
+
+        const imageWrapper = document.createElement("div"); 
+        imageWrapper.classList.add("image-wrapper");
     
         const title = document.createElement("h2");
         title.textContent = posts[i].acf.blog_title; 
         title.classList.add("h2-posts");
         element.append(title);
+        
 
         const image = document.createElement("img");
         image.src = posts[i].acf.blog_image;
         image.setAttribute("alt", posts[i].acf.alt_text); // REMEMBER ADDING ALT
         image.classList.add("posts-img");
-        element.append(image);
+        imageWrapper.append(image); 
+
+        element.append(imageWrapper);
+        
+        
+        const contentWrapper = document.createElement("div"); 
+        contentWrapper.classList.add("content-wrapper");
 
         const heading = document.createElement("h3");
         heading.textContent = posts[i].acf.blog_heading; 
         heading.classList.add("h3-posts");
-        element.append(heading);
+        contentWrapper.append(heading);
+
+        /*const paragraph1 = document.createElement('p');
+        paragraph1.textContent = posts[i].acf.blog_p1;
+        element.append(paragraph1);
+
+        const paragraph2 = document.createElement('p');
+        paragraph2.textContent = posts[i].acf.blog_p2;
+        element.append(paragraph2);
+
+        const paragraph3 = document.createElement('p');
+        paragraph3.textContent = posts[i].acf.blog_p3;
+        element.append(paragraph3); */
+
 
         const btn = document.createElement("a");
         btn.href = "/post/" + "?id=" + posts[i].id;
         btn.classList.add("button");
         btn.textContent = "Read More";
-        element.append(btn);
+        contentWrapper.append(btn);
 
+        element.append(contentWrapper);
 
+        
         parent.append(element);
+
     }
-    
-
-
-    
-} 
-
+}
+        
+        
